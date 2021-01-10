ede300c43e13fad76625971b1cc2e2fb74f37db7
dnejman <dawid.nejman@zalando.de>
2021-01-10 11:29:45 +0100

    Ex 2.3

--- exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedTest.scala
+++ exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedTest.scala
@@ -1,6 +1,6 @@
 package fpinscala.gettingstarted
 
-import fpinscala.gettingstarted.PolymorphicFunctions.isSorted
+import fpinscala.gettingstarted.PolymorphicFunctions.{curry, isSorted}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -12,6 +12,13 @@ class GettingStartedTest extends AnyWordSpec with Matchers {
       }
       "false for unsorted array" in {
         isSorted[Int](Array(2, 1, 3, 4), _ > _) shouldBe false
+      }
+    }
+    "curry" which {
+      "returns correctly curried function" in {
+        val result = curry[Int, Int, Int](_ + _)
+        result shouldBe a[Int => Int => Int]
+        result(1)(2) shouldBe 3
       }
     }
   }
