65d31a4fedea4774e55971b2082b79c4b906b871
dnejman <dawid.nejman@zalando.de>
2021-04-03 18:47:22 +0200

    Ex 3.3

--- exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
@@ -8,8 +8,24 @@ class ListTest extends AnyWordSpec with Matchers {
   private val provide = afterWord("provide")
 
   "List" should provide {
+
+    // Ex 3.1
     "x" which {
-      "is equal to x + y" in { List.x shouldBe 3 }
+      "provides right answer" in { List.x shouldBe 3 }
+    }
+
+    // Ex 3.2
+    "tail" which {
+      "removes first element from list" in { List.tail(Cons(1, Cons(2, Nil))) shouldBe Cons(2, Nil) }
+      "throws RuntimeException for empty list" in { assertThrows[RuntimeException](List.tail(Nil)) }
+    }
+
+    // Ex 3.3
+    "setHead" which {
+      "replaces the first element with different value" in {
+        List.setHead(Cons(1, Nil), 2) shouldBe Cons(2, Nil)
+      }
+      "throws RuntimeException for empty list" in { assertThrows[RuntimeException](List.setHead(Nil, 1)) }
     }
   }
 }
